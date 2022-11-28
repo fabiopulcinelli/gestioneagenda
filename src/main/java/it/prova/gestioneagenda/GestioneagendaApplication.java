ba92ff2580ed8cdaa573699ca427eed6f7d672df
fabiopulcinelli <fabiopulcinelli2k2@gmail.com>
2022-11-28 14:18:10 +0100

    primo commit

---
+++ src/main/java/it/prova/gestioneagenda/GestioneagendaApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestioneagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneagendaApplication.class, args);
+	}
+
+}
